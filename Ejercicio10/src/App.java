6ce072b16f317e450ead673330b9db505a68722e
Boris027 <titankiller027@gmail.com>
2023-10-04 20:04:50 +0200

    subida 3

--- Ejercicio10/src/App.java
+++ Ejercicio10/src/App.java
@@ -2,20 +2,20 @@ public class App {
     public static void main(String[] args) throws Exception {
 
             
-            System.out.println("***************");
+            System.out.println("\033[32m***************");
             System.out.println("*             *");
-            System.out.println("*   0    0    *                                               ");
+            System.out.println("*   \033[31m0    0 \033[32m   *                                               ");
             System.out.println("*      |      *                                          ");
             System.out.println("*             *                                               ");
-            System.out.println("*  (-----)    *                                                    ");
+            System.out.println("*  \033[31m(-----) \033[32m   *                                                    ");
             System.out.println("***************                                            ");
             System.out.println("                                                                           ");
-            System.out.println("r ¬                   _____    0                                                                                                                     ");
-            System.out.println("|   |     ______     |     )   _     ──────                                                                                                                    ");
-            System.out.println("| /      |  __  |    |____/   | |   │                                                                                                                    ");
-            System.out.println("| ¬      | |  | |    | \\\\     | |    ────                                                                                                                    ");
-            System.out.println("|   |    | |__| |    |  \\\\    | |        │                                                                                                                    ");
-            System.out.println("| /      |______|    |   \\\\   |_|   ─────                                                                                                                    ");
+            System.out.println("\033[35m ¬                    \033[31m_____    \033[35m0                                                                                                                     ");
+            System.out.println("\033[35m|   |   \033[36m  ______     \033[31m|     )   \033[35m_     \033[36m──────                                                                                                                    ");
+            System.out.println("\033[35m| /      \033[36m|  __  |    \033[31m|____/   \033[35m| |   \033[36m│                                                                                                                    ");
+            System.out.println("\033[35m| ¬      \033[36m| |  | |    \033[31m| \\\\     \033[35m| |   \033[36m ────                                                                                                                    ");
+            System.out.println("\033[35m|   |    \033[36m| |__| |    \033[31m|  \\\\    \033[35m| |   \033[36m     │                                                                                                                    ");
+            System.out.println("\033[35m| /      \033[36m|______|    \033[31m|   \\\\   \033[35m|_|   \033[36m─────                                                                                                                    ");
 
 
 
