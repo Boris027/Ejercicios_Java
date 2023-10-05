c0e004bdd113c015aa28d021792d387985bec9b4
Boris027 <titankiller027@gmail.com>
2023-10-05 19:18:09 +0200

    Tema 2 subida 4

--- Tema 2/src/Ejercicio6.java
+++ Tema 2/src/Ejercicio6.java
@@ -1,13 +1,14 @@
 public class Ejercicio6 {
     public static void main(String[] args) throws Exception {
     
-        float base=99f;
+        float base=10f;
         float resultado=21*base/100;
         float fin=base+resultado;
-        System.out.printf("%14s%6s%.2f %n","Base imponible","", base);
-        System.out.printf("%8s %11s%.2f%n","Iva(21%)","",resultado);
-        System.out.println("--------------------------");
-        System.out.printf("%5s%15s%.2f","Total","",fin);
+
+        System.out.printf("%-15s %6.2f %n","Base imponible", base);
+        System.out.printf("%-15s %6.2f %n","Iva(21%)",resultado);
+        System.out.println("-----------------------");
+        System.out.printf("%-15s %6.2f","Total",fin);
 
     }
 }
---
+++ Tema 2/src/Ejercicio7.java
@@ -0,0 +1,10 @@
+public class Ejercicio7 {
+    public static void main(String[] args) throws Exception {
+    
+       char letra='A';
+       String palabra="B";
+       System.out.println("Este es el valor declarado por char: "+letra+ " Este es el valor declarado por string: "+palabra); 
+       //No he tenido ningun propblema en declarar las dos variables de diferente tipo e intentar mostrarlas juntas en la misma linea usando println.
+
+    }
+}
