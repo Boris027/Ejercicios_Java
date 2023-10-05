bae090e6612acb449be950532e749a6155d31b1b
Boris027 <titankiller027@gmail.com>
2023-10-05 10:44:54 +0200

    Tema 2 subida 1

---
+++ Tema 2/src/Ejercicio1.java
@@ -0,0 +1,11 @@
+public class Ejercicio1 {
+    public static void main(String[] args) throws Exception {
+        int x=144;
+        int y=999;
+        System.out.println("Los numeros que se van a usar son: " +x +" y " +y);
+        System.out.println("La suma de " +y +" y " +x +" es " +(y+x));
+        System.out.println("La resta de " +y +" y " +x +" es " +(y-x));
+        System.out.println("La división de " +y +" entre " +x +" es " +((float)y/(float)x));
+        System.out.println("La multiplicación de " +y +" entre " +x +" es " +(y*x));
+    }
+}
---
+++ Tema 2/src/Ejercicio4.java
@@ -0,0 +1,9 @@
+public class Ejercicio4 {
+    public static void main(String[] args) throws Exception {
+        //Conversor de pesetas a euros
+        float euros=1f;
+        float conversion=166.386f;
+        System.out.printf("%.2f euros en pesetas son %.2f: ",euros, (euros*conversion));
+
+    }
+}
